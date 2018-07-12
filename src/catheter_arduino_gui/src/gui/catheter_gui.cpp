81f1900560103f1c82e253d1f4fedc390fcf4e4d
Samuel <xxx91@case.edu>
2018-07-12 12:53:33 -0400

    tested clicking software button multiple times

--- src/catheter_arduino_gui/src/gui/catheter_gui.cpp
+++ src/catheter_arduino_gui/src/gui/catheter_gui.cpp
@@ -19,7 +19,7 @@
 #include <iomanip>      // std::setprecision
 #include <fstream>
 #include <unistd.h>
-unsigned int microseconds = 100;
+unsigned int microseconds = 200;
 
 #include <wx/wfstream.h>
 #include <wx/numdlg.h>
@@ -225,13 +225,13 @@ void CatheterGuiFrame::OnSendCommandsButtonClicked(wxCommandEvent& e)
 
 void CatheterGuiFrame::OnSendPollButtonClicked(wxCommandEvent& e)
 {
-  //for(int itr = 0; itr<100; itr++)
-  //{
-  //std::cout << "current iteration: " << itr << std::endl;
+  for(int itr = 0; itr<100; itr++)
+  {
+  std::cout << "current iteration: " << itr << std::endl;
   sendPollCommand();
   setStatusText(wxT("Poll Command Successfully Sent"));
-  //usleep(microseconds);
-  //}
+  usleep(microseconds);
+  }
 }
 
 
