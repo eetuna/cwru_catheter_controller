e1ecbe83c8245431fc4d86574a225383f3daf1c9
Eser Erdem Tuna <eet12@case.edu>
2018-08-14 11:30:51 -0400

    split channels into two columns

--- src/catheter_arduino_gui/cmake-build-debug/CMakeFiles/run_tests_catheter_arduino_gui_roslint.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/run_tests_catheter_arduino_gui_roslint.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- src/catheter_arduino_gui/cmake-build-debug/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/usr/local/include;/usr/include;/usr/lib/x86_64-linux-gnu/wx/include/gtk2-unicode-3.0;/usr/include/wx-3.0".split(';') if "/usr/local/include;/usr/include;/usr/lib/x86_64-linux-gnu/wx/include/gtk2-unicode-3.0;/usr/include/wx-3.0" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lpc_utils_lib;-l:/usr/lib/x86_64-linux-gnu/libboost_system.so;-l:/usr/lib/x86_64-linux-gnu/libboost_thread.so;-l:/usr/lib/x86_64-linux-gnu/libboost_chrono.so;-l:/usr/lib/x86_64-linux-gnu/libboost_date_time.so;-l:/usr/lib/x86_64-linux-gnu/libboost_atomic.so;-l-L/usr/lib/x86_64-linux-gnu;-l-pthread;-l-lwx_gtk2u_core-3.0;-l-lwx_baseu-3.0;-l-lwx_gtk2u_adv-3.0;-l-lwx_gtk2u_propgrid-3.0".split(';') if "-lpc_utils_lib;-l:/usr/lib/x86_64-linux-gnu/libboost_system.so;-l:/usr/lib/x86_64-linux-gnu/libboost_thread.so;-l:/usr/lib/x86_64-linux-gnu/libboost_chrono.so;-l:/usr/lib/x86_64-linux-gnu/libboost_date_time.so;-l:/usr/lib/x86_64-linux-gnu/libboost_atomic.so;-l-L/usr/lib/x86_64-linux-gnu;-l-pthread;-l-lwx_gtk2u_core-3.0;-l-lwx_baseu-3.0;-l-lwx_gtk2u_adv-3.0;-l-lwx_gtk2u_propgrid-3.0" != "" else []
-PROJECT_NAME = "catheter_arduino_gui"
-PROJECT_SPACE_DIR = "/usr/local"
-PROJECT_VERSION = "0.0.1"
